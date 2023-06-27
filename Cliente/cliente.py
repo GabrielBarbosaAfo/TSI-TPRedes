af11929e40b529bf14b6a29a90af77b5dcb2b5d1
Gabriel Afonso Barbosa <gabrielafobarbosa@gmail.com>
2023-06-27 20:07:15 -0300

    Tratado as exceção com keyboard e TclError, além de fechar as threads abertas

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,6 +1,7 @@
 import socket
 import PySimpleGUI as sg
 import os
+from _tkinter import TclError
 
 client_socket = None  # Variável global para o socket do cliente
 window = None  # Variável global para a janela
@@ -19,11 +20,11 @@ def enviarEscolha(clienteSocket, escolha):
     escolhaCliente = int(escolha)
 
     # Enviar a escolha para o servidor
-    clienteSocket.sendall(str(escolhaCliente).encode())
+    clienteSocket.sendall(str(escolhaCliente).encode("utf-8"))
 
     while True:
         try:
-            resposta = clienteSocket.recv(1024).decode()
+            resposta = clienteSocket.recv(1024).decode("utf-8")
             if not resposta:
                 break
             _, arquivo = os.path.split(resposta)
@@ -44,51 +45,63 @@ def fecharConexao():
 
 def fecharJanela():
     fecharConexao()
-    window.close()
+    if window != None:
+        window.close()
 
 def iniciaCliente():
     global client_socket, window
     host = '127.0.0.1'
     porta = 12000
 
-    # Criação do socket e conexão com o servidor
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, porta))
+    try:
+        # Criação do socket e conexão com o servidor
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, porta))
+    
+    
+        # Receber a lista de imagens disponíveis
+        contadorImagem = int(client_socket.recv(1024).decode("utf-8"))
 
-    # Receber a lista de imagens disponíveis
-    contadorImagem = int(client_socket.recv(1024).decode())
+        nomeImagem = client_socket.recv(1024).decode("utf-8")
 
-    nomeImagem = client_socket.recv(1024).decode()
+   
+        # Layout da janela
+        layout = [
+            [sg.Text('Escolha uma opção:')],
+            [sg.Combo(nomeImagem.split("\r"), key='-ESCOLHA-', readonly=True, size=(20, 1))],
+            [sg.Button('Enviar')]
+        ]
 
-    # Layout da janela
-    layout = [
-        [sg.Text('Escolha uma opção:')],
-        [sg.Combo(nomeImagem.split("\n"), key='-ESCOLHA-', size=(20, 1))],
-        [sg.Button('Enviar')]
-    ]
+        # Criação da janela
+        window = sg.Window('Cliente', layout, size=(300, 100))
 
-    # Criação da janela
-    window = sg.Window('Cliente', layout, size=(300, 100))
+        # Loop de eventos
+        while True:
+            event, values = window.read()
 
-    # Loop de eventos
-    while True:
-        event, values = window.read()
+            if event == sg.WINDOW_CLOSED:
+                fecharJanela()
+                break
 
-        if event == sg.WINDOW_CLOSED:
-            fecharJanela()
-            break
-
-        if event == 'Enviar':
-            escolha = values['-ESCOLHA-']
-            i = 1
-            for nome in nomeImagem.split("\n"):
-                if escolha == nome:
-                    escolha = i
-                    break
-                else:
-                    i += 1
-            enviarEscolha(client_socket, escolha)
-            break
+            if event == 'Enviar':
+                escolha = values['-ESCOLHA-']
+                i = 1
+                for nome in nomeImagem.split("\r"):
+                    if escolha == nome:
+                        escolha = i
+                        break
+                    else:
+                        i += 1
+                enviarEscolha(client_socket, escolha)
+                break
+    except KeyboardInterrupt:
+        sg.popup('Ocorreu um erro', title='Erro')
+    except ConnectionRefusedError:
+        sg.popup('Não foi possível conectar ao servidor. Certifique-se de que o servidor esteja em execução.', title='Erro de Conexão')
+    except TclError:
+        print("Não há um display disponível para a interface gráfica do PySimpleGUI!!!")
+    finally:
+        fecharJanela()
 
 if __name__ == '__main__':
     try:
--- Server/server.py
+++ Server/server.py
@@ -1,35 +1,31 @@
 import socket
 import os
 import threading
-from os import sys
 
-# Abre o arquivo e envia a imagem escolhida pelo usuário
+# Open the file and send the chosen image to the client
 def enviaImagem(conexaoCliente, nomeArquivo):
     with open(nomeArquivo, 'rb') as file:
         arquivoLido = file.read()
         conexaoCliente.sendall(arquivoLido)
 
-# Envia a lista de imagens e recebe a resposta do usuário de qual imagem ele deseja
+# Send the list of images and receive the user's choice
 def trataCliente(conexaoCliente):
-    # Enviar a lista de imagens disponíveis
     imagens = os.listdir('imagens')
     quantidadeImagens = len(imagens)
-    conexaoCliente.send(str(quantidadeImagens).encode())
+    conexaoCliente.send(str(quantidadeImagens).encode("utf-8"))
 
     nomesImagens = ''
     for image_name in imagens:
-        nomesImagens += f"{image_name}\n"
+        nomesImagens += f"{image_name}\r"
 
-    conexaoCliente.send(nomesImagens.encode())
+    conexaoCliente.send(nomesImagens.encode("utf-8"))
 
     try:
-        # Receber a escolha do cliente
-        escolhaUsuario = int(conexaoCliente.recv(1024).decode())
+        escolhaUsuario = int(conexaoCliente.recv(1024).decode("utf-8"))
         imagemEscolhida = os.path.join('imagens', imagens[escolhaUsuario - 1])
-        conexaoCliente.sendall(imagemEscolhida.encode())
+        conexaoCliente.sendall(imagemEscolhida.encode("utf-8"))
         enviaImagem(conexaoCliente, imagemEscolhida)
-    except ValueError:
-        # Fechar a conexão
+    except (ValueError, KeyboardInterrupt):
         pass
     finally:
         conexaoCliente.close()
@@ -38,29 +34,34 @@ def iniciaServer():
     host = '127.0.0.1'
     porta = 12000
 
-    # Criação do socket e vincula do endereço e porta
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.bind((host, porta))
 
-    # O servidor entra em modo de escuta
     serverSocket.listen(1)
     print('Servidor aguardando conexões...')
 
+    # Lista para armazenar as threads ativas
+    thread_list = []
+
     while True:
         try:
-            # Aceitar uma nova conexão
             conexaoCliente, addr = serverSocket.accept()
             print('Cliente conectado:', addr)
 
-            # Tratar a conexão do cliente em uma nova thread
-            thread =  threading.Thread(target=trataCliente, args=(conexaoCliente,))
+            thread = threading.Thread(target=trataCliente, args=(conexaoCliente,))
+            thread_list.append(thread)
+            thread.daemon = True
             thread.start()
         except KeyboardInterrupt:
-            # Encerrar o servidor quando o usuário pressionar Ctrl+C
+            print("\nConexão Interrompida")
+            fecharThreads(thread_list)
             break
 
-    # Fechar o socket do servidor
     serverSocket.close()
+
+def fecharThreads(thread_list):
+    for thread in thread_list:
+        thread.join(0)
 
 if __name__ == '__main__':
     iniciaServer()
